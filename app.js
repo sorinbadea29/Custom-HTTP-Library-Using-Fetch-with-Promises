74fc0747c0834dd1955079fbfad9bad90595f20d
Sorin Badea <62335902+sorinbadea29@users.noreply.github.com>
2020-05-13 17:59:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,26 +15,27 @@ const dataToUpdateUser = {
 
 const http = new EasyHTTP;
 
+// Get Users
 http.get('https://jsonplaceholder.typicode.com/users')
   .then(data => console.log(data))
   .catch(err => console.log(err))
 
-
+// Get Single User
 http.get('https://jsonplaceholder.typicode.com/users/1')
   .then(data => console.log(data))
   .catch(err => console.log(err))
 
-
+// Create User
 http.post('https://jsonplaceholder.typicode.com/users', dataToCreateUser)
   .then(data => console.log(data))
   .catch(err => console.log(err))
 
-
+// Update User
 http.put('https://jsonplaceholder.typicode.com/users/2', dataToUpdateUser)
   .then(data => console.log(data))
   .catch(err => console.log(err))
  
-
+// Delete User
 http.delete('https://jsonplaceholder.typicode.com/users/5')
   .then(data => console.log(data))
   .catch(err => console.log(err))
